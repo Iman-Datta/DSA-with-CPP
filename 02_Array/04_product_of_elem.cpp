07fc9e0b50fc23f42dd1e0e112e627e05dfc1e4c
Iman-Datta <dattaiman56@gmail.com>
2025-08-05 00:17:01 +0530

    product of all element

---
+++ 02_Array/04_product_of_elem.cpp
@@ -0,0 +1,21 @@
+// Question :- Product of all numbers in array
+
+# include <iostream>
+using namespace std;
+
+int productArray(int arr[], int sz) {
+    int product = 1;
+    for (int i = 0; i < sz; i++){
+        product *= arr[i];
+    }
+    return product;
+}
+
+int main () {
+    int size = 7, product;
+    int arr[] = {1,2,3,4,5,6,7};
+
+    product = productArray(arr, size);
+    cout << "Product of all element of your array: " << product << endl;
+    return 0;
+}
---
+++ 02_Array/05_max_min_elem.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int max_min_Element (int arr[], int sz){
+
+}
+
+int main () {
+    int size = 8, max_element, min_element;
+    int arr[] = {1,2,3,8,4,5,6,7};
+    max_min_Element(arr, size);
+}
