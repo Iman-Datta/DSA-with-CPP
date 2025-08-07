5dbcb33e5c4e067bd191209cab762f47f61c63f4
Iman-Datta <dattaiman56@gmail.com>
2025-08-07 13:27:41 +0530

    1. product of all element 2. swap max and min

---
+++ 02_Array/05_Q2_swap_max_min_elem.cpp
@@ -0,0 +1,35 @@
+# include <iostream>
+using namespace std;
+
+void swap_max_min_Element (int arr[], int sz){
+    int max_indx = 0, min_indx = 0;
+    for (int i = 0; i < sz; i++){
+        if (arr[max_indx] < arr[i]){
+            max_indx = i;
+        }
+        if (arr[min_indx] > arr[i]) {
+            min_indx = i;
+        }
+    }
+    swap(arr[max_indx], arr[min_indx]);
+
+
+    for (int i = 0; i < sz; i ++) {
+        cout << arr[i] << "\t";
+    }
+    return;
+}
+
+int main () {
+    int size;
+    int arr[] = {1,2,3,8,4,5,6,7};
+    size = sizeof(arr)/sizeof(int);
+
+    cout << "Original Array: ";
+    for (int i = 0; i < size; i ++) {
+        cout << arr[i] << "\t";
+        }
+    cout << "\n";
+    cout << "After swaping the max and min element: ";
+    swap_max_min_Element(arr, size);
+}
